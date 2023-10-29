3499c786b2e3a2c77d51f9e99773506c214f3a8a
Rajesh P <rajesh.pethe@gmail.com>
2023-10-29 18:20:20 +0530

    MySQL service.

--- deploy-lamp-stack-on-k8/secret.sh
+++ deploy-lamp-stack-on-k8/secret.sh
@@ -3,4 +3,4 @@ kubectl create secret generic db-config \
     --from-literal=MYSQL_DATABASE=kodekloud \
     --from-literal=MYSQL_USER=kodekloud_user \
     --from-literal=MYSQL_PASSWORD=kodekloud_user \
-    --from-literal=MYSQL_HOST=db-service
+    --from-literal=MYSQL_HOST=mysql-service
