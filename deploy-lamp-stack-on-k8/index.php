d387ab7b42bf63536a1d6b7a3f21abd04acc9008
Rajesh P <rajesh.pethe@gmail.com>
2023-10-29 21:36:45 +0530

    Copy working PHP to container.

---
+++ deploy-lamp-stack-on-k8/index.php
@@ -0,0 +1,20 @@
+<?php
+$dbname = $_ENV["MYSQL_DATABASE"];
+$dbuser = $_ENV["MYSQL_USER"];
+$dbpass = $_ENV["MYSQL_PASSWORD"];
+$dbhost = $_ENV["MYSQL_HOST"];
+
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysqli_query($test_query);
+
+if ($result->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+  echo "Connected successfully";
+
+// Finally copy this file to target web container
+// kubectl cp -c httpd-php-container index.php lamp-wp-<replace-me>-abcd1:/app/index.php
+
+?>
