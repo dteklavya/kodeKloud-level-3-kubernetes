8f5e3b779d36cc86151d5002a04ff33a5ba6759e
Rajesh P <rajesh.pethe@gmail.com>
2024-05-05 12:46:39 +0530

    Minor fixes.

--- deploy-lamp-stack-on-k8/config-map.sh
+++ deploy-lamp-stack-on-k8/config-map.sh
@@ -1,5 +1,5 @@
-echo 'variables_order = "EGPCS"' > php.ini
-kubectl create configmap php-config --from-file php.ini
-# Verify
-kubectl get configmaps
-kubectl describe configmap php-config
+$ echo 'variables_order = "EGPCS"' > php.ini
+$ kubectl create configmap php-config --from-file php.ini
+$ # Verify
+$ kubectl get configmaps
+$ kubectl describe configmap php-config
--- deploy-lamp-stack-on-k8/create-dry-deployment.sh
+++ deploy-lamp-stack-on-k8/create-dry-deployment.sh
@@ -1,3 +1,3 @@
-kubectl create deployment lamp-wp \
+$ kubectl create deployment lamp-wp \
     --image=webdevops/php-apache:alpine-3-php7 \
     --dry-run=client -o yaml > lamp-wp.yaml
